e669569a983e2cd053014a5410022e5f7d44e889
sailBeaut <beau.sebkes@gmail.com>
2026-05-04 23:40:52 +0200

    hopefully render works now?

--- app/api.py
+++ app/api.py
@@ -33,7 +33,8 @@ app = FastAPI(title="Stock Advisor", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
